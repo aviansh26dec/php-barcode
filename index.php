3c7aa70c67ae113535d805cd636896768c3bf433
aviansh26dec <avinashkumarsingh.ignou@gmail.com>
2017-01-30 11:23:35 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Barcode</title>
+</head>
+<body>
+	<img alt="12345" src="barcode.php?codetype=code128&size=40&text=2939-gram&print=true" />
+</body>
+</html>
